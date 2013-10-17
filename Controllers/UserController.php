7068ee8a0747579d45fddfcbcd091565248bae40
Nicolas Portier <nicola.portier@gmail.com>
2013-10-17 23:49:18 +0200

    auth

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -4,7 +4,7 @@
 	{
 		public function authAction()
 		{
-			if (!empty($_POST) && !empty($_POST['pseudo'])) {
+			if (!empty($_POST['pseudo'][0]) && !empty($_POST['pseudo'][1])) {
 				$players = array(
 					"playerOne" => User::Find(array("pseudo" => $_POST['pseudo'][0])),
 					"playerTwo" => User::Find(array("pseudo" => $_POST['pseudo'][1])),
@@ -12,11 +12,13 @@
 
 				foreach ($players as $key => $player) {
 					if ($player != NULL) {
-
+						
 					} else {
-
+						
 					}
 				}
+			} else {
+
 			}
 		}
 	}

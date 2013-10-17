4c74f10391204341b04d27382c5e7fb912512197
Baptiste Gios <baptiste.gios@supinternet.fr>
2013-10-18 01:08:46 +0200

    class tank start

--- Classes/Warrior.php
+++ Classes/Warrior.php
@@ -1,10 +1,11 @@
 <?php
+
 class Warrior extends Champion {
 
     function __construct()
     {
         parent::__construct(array('strength' => 60, 'intelligence' => -50, 'health' => 0));
-        $this->add_weapons(new Weapon(array('name' => 'Axe of doom','strength' => 10)));
+        $this->add_weapons(new Weapon(array('name' => 'Axe of doom','strength_bonus' => 10)));
     }
 
     public function secondaryComp(Champion $enemy)

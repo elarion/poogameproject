edfeab6a3bba0b9554fd785fbec252a8628c95c8
Baptiste Gios <baptiste.gios@supinternet.fr>
2013-10-18 18:45:45 +0200

    Views

---
+++ templates/waiting.php
@@ -0,0 +1,54 @@
+
+<header>
+    <h1>
+        <img src="img/logoPOO.png">
+    </h1>
+</header>
+<?php echo($var['user1']->pseudo); ?> (<?php echo($var['champion_user1'][0]->name); ?>)
+<span class="hp"><?php echo($var['champion_user1'][0]->health); ?>HP</span>
+
+<div id="form_wrapper">
+    <span id="players_waiting">Players waiting to fight !</span>
+    <ul id="list_players">
+        <li>
+            Player01
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player02
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player03
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player04
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player05
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player06
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player07
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player08
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player09
+            <a href="">DÉFIER</a>
+        </li>
+        <li>
+            Player10
+            <a href="">DÉFIER</a>
+        </li>
+    </ul>
+</div>
